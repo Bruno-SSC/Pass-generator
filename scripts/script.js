a35ab88c549bc588690a82a70fc4c4e13dbc218b
Bruno <faithkidbruno@gmail.com>
2022-10-27 18:05:27 -0300

    cleaning

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,8 @@
 let sliderElement = document.querySelector("#slider")
-
 let buttonElement = document.querySelector("#button")
 
 let sizePass = document.querySelector("#valor")
 let password = document.querySelector("#password")
-
-let containerPassword = document.querySelector("#container-password")
 
 let charset = "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM0123456789"
 
@@ -13,22 +10,23 @@ let novaSenha = ""
 
 sizePass.innerHTML = sliderElement.value
 
-sliderElement.oninput = function(){
+sliderElement.oninput = function () {
     sizePass.innerHTML = this.value
 }
 
-function generatePass(){
+function generatePass() {
+    let containerPassword = document.querySelector("#container-password")
+    console.log(containerPassword)
     let pass = ""
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
         pass += charset.charAt(Math.floor(Math.random() * n))
     }
-    
     containerPassword.classList.remove('hide')
-    password.innerHTML = pass  
+    password.innerHTML = pass
     novaSenha = pass
 }
 
-function copyPass(){
+function copyPass() {
     alert("senha copiada com sucesso")
     navigator.clipboard.writeText(novaSenha)
 }
